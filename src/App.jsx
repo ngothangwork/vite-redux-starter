60656a0e53d41f36ce6fe4b60ac6ba38ce8990eb
ThangNgo <tngo1038@gmail.com>
2025-05-26 05:51:45 +0700

    First commit with Redux and Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './component/Counter.jsx';
+
+function App() {
+    return (
+        <div>
+            <h1>Redux Toolkit + Vite</h1>
+            <Counter />
+        </div>
+    );
+}
+export default App;
